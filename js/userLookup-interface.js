62e6b87a589be66c5ca15e392498274780c8ffd4
vp <notejde@gmail.com>
2016-10-14 16:27:15 -0700

    update js

--- js/userLookup-interface.js
+++ js/userLookup-interface.js
@@ -1,14 +1,39 @@
-var Repos = require('./../js/userLookup-interface.js').reposModule;
+var apiKey = "1482ae2b901b63efd5fde17a4f40226057c08c3e";
 
 $(document).ready(function() {
-  var userNameRepos = new Repos();
   $('#searchName').click(function() {
-    //get value from form and make any input into lowerCase
-    var name = $('#userName').val().toLowerCase();
+    //get input value
+    var name = $('#userName').val();
     $('#userName').val("");
-    //Repos.prototype.getRepos = function(methodParameter){getRepos code}
-    $('.showUser').text("The username you chose is " +name+ ".");
-
+    $.get('https://api.github.com/users/talayruk?access_token=' + apiKey, function(response) {
+      console.log(response);
+      $('.showUser').append("The name you have chosen is " + name +  "." + <li> + "The picture profile is " + response.login + ", " + response.avatar_url + " , " + response. html_url " .");
+      $('#userPic').show();
+    });
   });
-
 });
+
+
+
+
+
+
+
+
+
+
+
+// var Repos = require('./../js/userLookup-interface.js').reposModule;
+//
+// $(document).ready(function() {
+//   var userNameRepos = new Repos();
+//   $('#searchName').click(function() {
+//     //get value from form and make any input into lowerCase
+//     var name = $('#userName').val().toLowerCase();
+//     $('#userName').val("");
+//     //Repos.prototype.getRepos = function(methodParameter){getRepos code}
+//     $('.showUser').text("The username you chose is " +name+ ".");
+//
+//   });
+//
+// });
--- js/userLookup.js
+++ js/userLookup.js
@@ -1,18 +1,18 @@
-var apiKey = require('./../.env').apiKey;
-
-Repos = function() {
-
-}
-
-
-Repos.prototype.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
-  });
-};
-
-
-
-exports.reposModule = Repos;
+// var apiKey = require('./../.env').apiKey;
+//
+// Repos = function() {
+//
+// };
+//
+//
+// Repos.prototype.getRepos = function(){
+//   $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+//     console.log(response);
+//   }).fail(function(error){
+//     console.log(error.responseJSON.message);
+//   });
+// };
+//
+//
+//
+// exports.reposModule = Repos;
