e9eeb83f6cfa007781cf8dcffc84efc80815246b
vp <notejde@gmail.com>
2016-10-15 06:17:16 -0700

    add list out repo is not working only link to the user works

--- js/repos-interface.js
+++ js/repos-interface.js
@@ -1,10 +1,14 @@
 var Repos = require('./../js/repos.js').reposModule;
 
-var displayData = function(name, userData) {
-  $('.showUser').append("<h2> Here is the username " + userData + ". </h2>");
-  console.log(userData);
-};
+var displayData = function(name, userData, reposData, moreData) {
+  $('.showUser').append("<h2> The username " + name + " is found as " + userData + ". </h2>");
 
+  $('#showRepos').append("<h2><a href=" + reposData + ">Click here to see " + name + " repositories.</a>" + "</h2>");
+
+  // $('ul#repoUrl').append("<li><a href=" + moreData + ">Click here to see " + name + " repositories.</a>" + "</li>"); can not get ..Undefined
+
+  console.log(userData, " ", reposData + " YES");
+};
 
 $(document).ready(function() {
   var searchRepos = new Repos();
--- js/repos.js
+++ js/repos.js
@@ -6,14 +6,16 @@ Repos = function() {
 
 Repos.prototype.getRepos = function(name, displayFunction){
   $.get('https://api.github.com/users/' + name + '?access_token=' + apiKey).then(function(response){
-    // console.log(response);
-    displayFunction(name, response.login);
-    console.log(response.login + "yes");
+    console.log(response);
+    displayFunction(name, response.login, response.html_url, response.repo_url);
+    console.log(response.login + " " + response.html_url);
   }).fail(function(error){
-    $('.showUser').text("This Username Is " + error.responseJSON.message + "." +
+    $('.showUser').text("This Username " + name + " is " + error.responseJSON.message + "." +
     "Please Enter the Correct Username");
   });
 };
 
 
 exports.reposModule = Repos;
+
+//write Get repos method
